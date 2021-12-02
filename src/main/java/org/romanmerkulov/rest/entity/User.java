e4b86569dd84783ccb7829c1a7fd75cfc0775eba
Roman Merkulov <merkulov.romka@gmail.com>
2021-12-02 13:12:41 +0300

    3.1.4

---
+++ src/main/java/org/romanmerkulov/rest/entity/User.java
@@ -0,0 +1,15 @@
+package org.romanmerkulov.rest.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Long id;
+    private String name;
+    private String lastName;
+    private Byte age;
+}
